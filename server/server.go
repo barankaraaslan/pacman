5c5e1cddb181edb416b148e7a765b5590b915425
Baran Karaaslan <barankaraaslan@protonmail.com>
2022-07-03 20:16:18 +0300

    add --dir optional flag to change directory to serve packages from

--- server/server.go
+++ server/server.go
@@ -3,10 +3,21 @@ package server
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"strings"
 )
 
+func get_directory_to_serve_from_args() string {
+	for index, arg := range os.Args {
+		if strings.HasPrefix(arg, "--dir") {
+			return os.Args[index + 1]
+		}
+	}
+	return "packages-to-serve/"
+}
+
 func Server() error {
-	fs := http.FileServer(http.Dir("packages-to-serve/"))
+	fs := http.FileServer(http.Dir(get_directory_to_serve_from_args()))
 
 	port := ":5001"
     fmt.Println("Server is running on port" + port)
