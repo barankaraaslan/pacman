c2fdc34c9a4d680a0ab6d990259c4e47dec86ac0
Baran Karaaslan <barankaraaslan@protonmail.com>
2022-07-03 20:05:03 +0300

    add --version functionality

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"pacman/pkg"
 	"pacman/server"
 )
+
+const version = "0.0.1"
 
 func main() {
 	if len(os.Args) == 2 {
@@ -15,6 +18,11 @@ func main() {
 	if len(os.Args) == 2 {
 		if os.Args[1] == "server" {
 			server.Server()
+		}
+	}
+	for _, args := range os.Args {
+		if args == "--version" {
+			fmt.Println(version)
 		}
 	}
 }
